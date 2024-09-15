add1f83570b8f5800fc15907f47d4afea9a55d2c
Rahul-2822 <rahul15sheoran@gmail.com>
2024-09-15 17:28:34 +0530

    Add files via upload

---
+++ staffList.js
@@ -0,0 +1,33 @@
+import img1 from './1.jpeg';
+import img2 from './2.jpeg';
+import img3 from './6.jpg';
+import { faker } from '@faker-js/faker'
+const staffList =[{
+    key:0,
+    avatar : img1,
+    name : 'Old Trafford',
+    description : 'Manchester United football ground.',
+    star : '🌟5',
+    Author : 'Archibald Leitch'
+},
+{
+    key:1,
+    avatar : img2,
+    name : '  FA Cup  ',
+    description : "Engalnd's biggest football competition",
+    star : '🌟5',
+    Author : 'Charles W. Alcock'
+},
+{
+    key:2,
+    avatar : img3,
+    name : 'Manchester United',
+    description : "Logo",
+    star : '🌟4',
+    Author : 'Sir Alex Ferguson'
+},
+
+
+
+]
+export default staffList
---
+++ tutList.js
@@ -0,0 +1,31 @@
+import img4 from './3.jpeg'
+import img5 from './4.jpeg'
+import img6 from './5.jpeg'
+
+const tutList = [{
+    key:0,
+    avatar : img4,
+    name : 'World Cup trophy',
+    description : 'Biggest football competetion in world.',
+    star : '🌟4',
+    Author : 'Jules Rimet'
+},
+{
+    key:1,
+    avatar : img5,
+    name : 'Argentina ',
+    description : " Won world cup in 2024",
+    star : '🌟5',
+    Author : 'Lionel Scaloni'
+},
+{
+    key:2,
+    avatar : img6,
+    name : 'Brazil',
+    description : "Has won most world cup's in histroy",
+    star : '🌟4',
+    Author : 'Pele'
+},
+]
+
+export default tutList
